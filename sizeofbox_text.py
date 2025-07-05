c4df59c4c2021d713c4fa3ca7c51615c195eb289
ISHIKA <ishikaudvanshi@gmail.com>
2025-07-05 22:30:47 +0530

    Add files via upload

---
+++ sizeofbox_text.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root=Tk()
+#width x height
+root.geometry("44x434")   # resize kr skte h usko ..
+#width , height 
+root.minsize(200,100)     #min size fix ho gya h
+root.maxsize(500,200)
+value=Label(text="hey ..i am ishika and this is my GUI ")
+value.pack()
+root.mainloop()
