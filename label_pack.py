e39824b9e85e00b659193f5ed93c7e6677adaeca
ISHIKA <ishikaudvanshi@gmail.com>
2025-07-05 22:55:26 +0530

    Add files via upload
    use of attribute of label and pack

---
+++ label_pack.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root=Tk()
+root.geometry("734x654")
+root.title("Welcome to ISHIKA GUI")
+title=Label(text='''Dad sees a boy running in his bedroom. \n I don’t see the boy. But I do see my bedbound dad’s anxiety. He wants the boy to go play outside, under the coolness of trees. \n I take cues from the hospice nurse:\n how she pinched the air where annoying invisible candles flickered in another patient’s room, extinguishing the unseen wicks. \n I’ve grasped the boy’s hand to lead him out, I say. \nI imagine the boy tugs on my arm in the entryway as his other hand reaches for the front door. Weary, I imagine a time when I don’t let go.''',
+bg="pink" ,fg="red" ,padx=10,pady=19  ,borderwidth=8 ,relief=SUNKEN)
+title.pack(anchor="sw",side=BOTTOM )
+root.mainloop()
