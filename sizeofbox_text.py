0766498c5040cf888a1cfabadd8a954ece59c037
ISHIKA <ishikaudvanshi@gmail.com>
2025-07-06 07:41:11 -0400

    Update sizeofbox_text.py

--- sizeofbox_text.py
+++ sizeofbox_text.py
@@ -1,10 +1,10 @@
 from tkinter import *
 root=Tk()
 #width x height
-root.geometry("44x434")   # resize kr skte h usko ..
+root.geometry("44x434")   
 #width , height 
-root.minsize(200,100)     #min size fix ho gya h
-root.maxsize(500,200)
+root.minsize(200,100)     #min size fix 
+root.maxsize(500,200)     #max siz fix
 value=Label(text="hey ..i am ishika and this is my GUI ")
 value.pack()
 root.mainloop()
